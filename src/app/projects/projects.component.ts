25cf3b650f9bd8e732317157550aaafdc24041b7
Darkmet98 <darkmet98@gmail.com>
2020-06-07 18:13:30 +0200

    :hear_no_evil: Implemented pagination on projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -11,20 +11,33 @@ import {DataService} from "../data.service";
 
 export class ProjectsComponent implements OnInit {
   projects: Project[];
-
+  page : number = 0;
+  size : number = 0;
 
   constructor(public router: Router, public dataService: DataService) { }
 
   ngOnInit(): void {
     const json = {
+      "page" : 1,
       id: JSON.parse(localStorage.getItem("user")).Id
     };
     this.dataService.sendPostRequest("/api/projects", json).subscribe((data: any[])=> {
-      this.projects = data;
+      this.projects = data["Projects"];
+      this.size = data["NumberPages"];
     });
   }
 
   public GoTo(id) {
     this.router.navigateByUrl('/projects/' + id);
+  }
+
+  public getData(obj) {
+    const json = {
+      "page" : obj.pageIndex+1,
+      id: JSON.parse(localStorage.getItem("user")).Id
+    }
+    this.dataService.sendPostRequest("/api/projects", json).subscribe((data: any[])=> {
+      this.projects = data["Projects"];
+    });
   }
 }
